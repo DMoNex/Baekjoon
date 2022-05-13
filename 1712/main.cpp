4add0f6ee4bbec40352e8fe7359236b24c89f5c6
DMoNex <68813204+DMoNex@users.noreply.github.com>
2022-05-13 11:45:13 +0900

    tag: math

---
+++ 1712/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B, C;
+	int cost = 0;
+
+	cin >> A >> B >> C;
+
+	cost = A;
+
+	if (B >= C) {
+		cout << -1;
+		return 0;
+	}
+
+	cout << A / (C - B) + 1;
+}
