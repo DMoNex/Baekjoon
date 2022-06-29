d96927b312e0ac174954e8085a8ed231505e49fe
DMoNex <68813204+DMoNex@users.noreply.github.com>
2022-06-29 12:11:32 +0900

    tag: math

---
+++ 5522/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int score[5];
+	
+	cin >> score[0] >> score[1] >> score[2] >> score[3] >> score[4];
+	cout << score[0] + score[1] + score[2] + score[3] + score[4];
+}
