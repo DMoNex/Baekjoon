e87ef0f1fe00476793a670e148b0fe7dfa8e08a9
DMoNex <68813204+DMoNex@users.noreply.github.com>
2022-10-23 15:52:22 +0900

    tag: implement

---
+++ 1000/2522/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = N-1; i > 0; --i) {
+		for (int j = i; j > 0; --j)
+			cout << ' ';
+		for (int k = 0; k < N - i; ++k)
+			cout << '*';
+		cout << '\n';
+	}
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < i; ++j)
+			cout << ' ';
+		for (int k = 0; k < N - i; ++k)
+			cout << '*';
+		cout << '\n';
+	}
+}
