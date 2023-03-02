7058ffe8303db3dc58b63855053dd3828d7cf74b
DMoNex <68813204+DMoNex@users.noreply.github.com>
2023-03-02 21:11:49 +0900

    tag: implement, string

---
+++ 10000/10808/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int count[26] = { 0 };
+	string input;
+	cin >> input;
+
+	for (int i = 0; i < input.size(); ++i)
+		count[input[i] - 'a']++;
+
+	for (int i = 0; i < 26; ++i)
+		cout << count[i] << ' ';
+}
