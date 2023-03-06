9f72d8321da51ed011ef44c45f6bb011008bea8f
DMoNex <68813204+DMoNex@users.noreply.github.com>
2023-03-06 21:32:44 +0900

    tag: sort

---
+++ 1000/2693/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int T;
+	int arr[10] = { 0 };
+
+	cin >> T;
+	while (T--) {
+		for (int i = 0; i < 10; ++i)
+			cin >> arr[i];
+		
+		sort(arr, arr + 10);
+
+		cout << arr[7] << '\n';
+	}
+}
