22ff0987b5b0c85646d74bbdc6141badc0c1238d
DMoNex <68813204+DMoNex@users.noreply.github.com>
2024-08-27 13:45:16 +0900

    tag: implement

---
+++ 10000/25314/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    while(n > 0) {
+        n -= 4;
+        
+        cout << "long ";
+    }
+    
+    cout << "int";
+    
+    return 0;
+}
